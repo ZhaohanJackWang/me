58be4197eeeda1ac9c76ad3a6a6501437f3fc781
Jack Wang <wang_ja@Whack-Jang.local>
2020-06-17 11:04:26 +1000

    week 3

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,14 @@ def loop_ranger(start, stop=None, step=1):
     The look up the docs for range(), you can answer this with just the range 
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+
+    a_list = []
+    x = start
+    while x < stop:
+        a_list.append(x)
+        print(x)
+        x = x + step
+    return a_list
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +27,8 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    
+    return range(start, stop, step)
 
 
 def two_step_ranger(start, stop):
@@ -29,7 +37,7 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
+    return range(start, stop, 2)
 
 
 def stubborn_asker(low, high):
--- week3/readme.md
+++ week3/readme.md
@@ -1,1 +1,11 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+catching error:
+
+try:
+    do_a_dumb()
+except Exception as e:
+    print("you did a dumb", e)
+finally:
+    print("we still love you")
+
+
